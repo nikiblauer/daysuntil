871a30bcf318490bfe2ed798153aa38c2a35c613
Niklas Gruener <niki.gruener@gmail.com>
2021-11-21 17:00:48 +0100

    Added proper delete functionality, added icon, cleaned code

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+//libraries
 const express = require("express");
 const ejs = require("ejs");
 const bodyParser = require("body-parser");
@@ -6,139 +6,169 @@ const _ = require('lodash');
 const mongoose = require("mongoose");
 
 
+//server initialization
 const app = express();
 const port = 3000;
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
 app.set("view engine", "ejs");
 
 
+//mongoose initialization
 mongoose.connect("mongodb://localhost:27017/daysuntilDB");
 const daySchema = new mongoose.Schema({
   name: String,
   date: String
 });
+const DayModel = mongoose.model("Day", daySchema); //used for creating new documents in database
 
-const DayModel = mongoose.model("Day", daySchema);
-const testDay1 = new DayModel({name: _.startCase("Neujahr"), date: "01-01-2022"});
-const testDay2 = new DayModel({name: _.startCase("Geburtstag"), date: "12-15-2021"});
-const testDay3 = new DayModel({name: _.startCase("Sommerferien"), date: "06-07-2022"});
-
-const defaultDays = [testDay1, testDay2, testDay3];
-
-// DayModel.insertMany(defaultDays, function(err){
-//   if(err){
-//     console.log(err);
-//   } else {
-//     console.log("Successfully saved default days to DB.");
-//   }
-// });
-
-// DayModel.find({}, function(err, docs) {
-//   if(err){
-//     console.log(err);
-//   } else {
-//     docs.forEach(function(doc){
-//       console.log(doc.name);
-//     });
-//   }
-// });
-
-function DaysUntilDate(name, date){
-  this.name = name;
-  this.date = date;
-}
-
+//helper function - convert calender date to days left
 function getDays(date) {
-  let now = new Date();
-  let endDate = new Date(date);
-  let differenceInTime = (endDate.getTime() - now.getTime());
-  let days = differenceInTime / (1000*60*60*24);
+  const now = new Date();
+  const endDate = new Date(date);
+  let differenceInTime = (endDate.getTime() - now.getTime()); //calculate difference between dates in milliseconds
+  const days = Math.round((differenceInTime / (1000 * 60 * 60 * 24))); //convert milliseconds to days
 
 
-  return Math.round(days);
+  return days;
 }
 
-let daysList = [];
-daysList.push(new DaysUntilDate(_.startCase("Neujahr"), "01-01-2022"));
-daysList.push(new DaysUntilDate(_.startCase("Geburtstag"), "12-15-2021"));
-daysList.push(new DaysUntilDate(_.startCase("Sommerferien"), "06-07-2022"));
+////////////////////////////////////////////////////////////////////////////
+// default route
+////////////////////////////////////////////////////////////////////////////
+app.get("/", function(req, res) {
+  // Check for deletion query string in URL -> e.g. http://daysuntil.com/?delete=abcDate
+  const deleteQuery = _.startCase(req.query.delete);
+  if (deleteQuery.length > 0) {
 
-
-
-
-app.get("/", function(req, res){
-  DayModel.find({}, function(err, docs){
-    if(err){
-      console.log(err);
-    } else{
-
-      let currentDay = docs[0];
-      currentDay.date = getDays(currentDay.date);
-
-      const options = {
-        currentDay: currentDay,
-        daysList: docs,
-        dateToday: new Date()
-      };
-
-      res.render("pages/index", options);
-    }
-  });
-
-});
-
-app.get("/:name", function(req, res){
-  DayModel.find({}, function(err, docs){
-    if(err){
-      console.log(err);
-    } else{
-
-      const dayName = _.startCase(req.params.name);
-
-      let currentDay = docs[0];
-
-      for( var i=0; i<docs.length; i++){
-        if(dayName === docs[i].name){
-          currentDay = docs[i];
-          break;
+    DayModel.deleteOne({
+      name: deleteQuery
+    }, function(err, result) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (result.deletedCount > 0) {
+          console.log("Deleted: " + deleteQuery);
+        } else {
+          console.log("Couldn't find entry: " + deleteQuery);
         }
       }
+    });
 
-      currentDay.date = getDays(currentDay.date);
-
-      const options = {
-        currentDay: currentDay,
-        daysList: docs,
-        dateToday: new Date()
-      };
-
-      res.render("pages/index", options);
-    }
-  });
-});
-
-app.listen(port, function(){
-  console.log("Server started on port: " + port);
-});
-
-app.post("/", function(req, res){
-  if(req.body.dateName === ""){
     res.redirect("/");
     return;
   }
+
+  //Display first element of daysuntil-database when no specific route is given
+  DayModel.find({}, function(err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+
+      let currentday = null;
+      let options = null;
+
+      //check if database already contains entries
+      if (docs.length > 0) {
+        currentDay = docs[0]; // select first document in database
+        currentDay.date = getDays(currentDay.date); //convert date to days-left
+
+        options = {
+          currentDay: currentDay,
+          daysList: docs,
+          dateToday: new Date()
+        };
+
+      } else { // if not, send null-option to ejs
+        options = {
+          currentDay: null,
+          daysList: null,
+          dateToday: null
+        }
+      }
+
+      res.render("pages/index", options); // render page
+    }
+  });
+
+});
+
+////////////////////////////////////////////////////////////////////////////
+//display entry when specific name is given
+////////////////////////////////////////////////////////////////////////////
+app.get("/:name", function(req, res) {
+  DayModel.find({}, function(err, docs) {
+    if (err) {
+      console.log(err);
+    } else {
+
+      let currentDay = null; // init currentDay with null
+      let options = null;
+
+      if (docs.length > 0) {
+        const dayName = _.startCase(req.params.name); // extract dayname from url
+        // check if dayname is in database, if yes currentDay = doc, if not currentDay = null
+        for (var i = 0; i < docs.length; i++) {
+          if (dayName === docs[i].name) {
+            currentDay = docs[i];
+            break;
+          }
+        }
+        // if currentDay === null, redirect to default route
+        if (currentDay === null) {
+          res.redirect("/");
+          return;
+        }
+
+        currentDay.date = getDays(currentDay.date); //convert date to days-left
+
+        options = {
+          currentDay: currentDay,
+          daysList: docs,
+          dateToday: new Date()
+        };
+
+        res.render("pages/index", options);
+
+      } else { // if no docs in database redirect to default route and handle the problem there
+        res.redirect("/");
+        return;
+      }
+    }
+  });
+});
+
+
+
+////////////////////////////////////////////////////////////////////////////
+// create new days
+////////////////////////////////////////////////////////////////////////////
+app.post("/", function(req, res) {
+  // check if name was given, if not redirect to home route
+  if (req.body.dateName === "") {
+    res.redirect("/");
+    return;
+  }
+
+  // parse form inputs
   const dateName = _.startCase(req.body.dateName);
   const date = req.body.date;
 
-  let daysUntilDate = new DaysUntilDate(dateName, date);
 
-
-  DayModel.find({name: dateName}, function(err, docs){
-    if(err){
+  // check if name is already in use
+  DayModel.find({
+    name: dateName
+  }, function(err, docs) {
+    if (err) {
       console.log(err);
     } else {
-      if(docs.length === 0){
-        let newDay = new DayModel({name: dateName, date: date});
+      if (docs.length === 0) { // if not already used create new day
+        let newDay = new DayModel({
+          name: dateName,
+          date: date
+        });
         newDay.save();
       } else {
         console.log("Date already in list.");
@@ -146,7 +176,13 @@ app.post("/", function(req, res){
     }
   });
 
-
-
   res.redirect("/" + dateName);
+});
+
+
+////////////////////////////////////////////////////////////////////////////
+// listen for incoming connections
+////////////////////////////////////////////////////////////////////////////
+app.listen(port, function() {
+  console.log("Server started on port: " + port);
 });
--- public/navigation.js
+++ public/navigation.js
@@ -36,12 +36,46 @@ function cutUrl(url,n){
     return url.split('/').slice(0,n).join('/');
 }
 
+var toggled = 0;
+
 
 $(".list-item").on("click", function(e){
+
+
+
   let name = event.target.innerHTML;
   name = name.substring(1, name.length-1);
   let url = cutUrl(window.location.href, 3);
-  url = url + "/" + name;
 
-  window.location.replace(url);
+  if($(".list-item").hasClass("delete")){
+    if (window.confirm('Willst du den Tag: ' + '"' + name + '"' + ' wirklich löschen?')) {
+      url = url + "/?delete=" + name;
+      toggled = 0;
+      $(".list-item").removeClass("delete");
+      $(".edit-btn").removeClass(".edit-btn-toggled");
+
+      window.location.replace(url);
+    } else {
+
+    }
+
+  } else {
+    url = url + "/" + name;
+    window.location.replace(url);
+  }
+
+});
+
+
+$(".edit-btn").on("click", function(e){
+  if(toggled === 0){
+    $(".list-item").addClass("delete");
+    $(".edit-btn").addClass("edit-btn-toggled");
+
+    toggled = 1;
+  } else {
+    $(".list-item").removeClass("delete");
+    $(".edit-btn").removeClass("edit-btn-toggled");
+    toggled = 0;
+  }
 });
